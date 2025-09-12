c9e3f696d06dbd8c03d5a07060a5b2ed01a7d0a3
zandk <emasande83@gmail.com>
2025-09-12 12:44:08 -0300

    primer avance

---
+++ entrega-react-1/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+export const Navbar = () => (
+    <>
+        <ul>
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <img src="" alt="" />
+                    <a class="navbar-brand">Pizza Di Sande</a>
+                    <a>ggg</a>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        </ul></>
+)
