c75db9e641305d1eae1cd1bf63ca4499bc2d8281
zandk <emasande83@gmail.com>
2025-09-16 12:59:58 -0300

    avance

--- entrega-react-1/src/components/Nav.jsx
+++ entrega-react-1/src/components/Nav.jsx
@@ -1,16 +1,38 @@
+import logo from "../img/logo-pizzeria.png"
+
 export const Navbar = () => (
     <>
-        <ul>
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
-                    <img src="" alt="" />
-                    <a class="navbar-brand">Pizza Di Sande</a>
-                    <a>ggg</a>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+        <header>
+            <nav>
+
+                <div>
+                    <img src={logo} alt="logo pizzeria" />
+                </div>
+
+                <div>
+                    <ul class="navbar bg-body-tertiary">
+
+                        <li>
+                            <h2 class="navbar-brand">Pizza Di Sande</h2>
+                        </li>
+
+                        <li>
+                            <h3>Cat Pizzas</h3>
+                        </li>
+
+                        <li>
+                            <h3>Cat Empanadas</h3>
+                        </li>
+
+                        <li>
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="Buscar" placeholder="Buscar" aria-label="Buscar" />
+                                <button class="btn btn-outline-success" type="submit">Buscar</button>
+                            </form>
+                        </li>
+
+                    </ul>
                 </div>
             </nav>
-        </ul></>
+        </header></>
 )
