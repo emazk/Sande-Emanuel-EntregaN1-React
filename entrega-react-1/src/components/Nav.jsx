81e24e03cd80e10c3469dcb5678377dfad53fef0
zandk <emasande83@gmail.com>
2025-09-18 12:47:36 -0300

    terminado

--- entrega-react-1/src/components/Nav.jsx
+++ entrega-react-1/src/components/Nav.jsx
@@ -1,36 +1,31 @@
 import logo from "../img/logo-pizzeria.png"
+import "./Nav.css"
 
 export const Navbar = () => (
     <>
         <header>
-            <nav>
+            <nav className="nav">
+                <div className="container">
+                    <div>
+                        <a href="https://unsplash.com/es/fotos/gatito-atigrado-marron-sentado-en-el-suelo-nKC772R_qog"><img className="logo" src={logo} alt="logo pizzeria" /></a>
+                    </div>
 
-                <div>
-                    <img src={logo} alt="logo pizzeria" />
-                </div>
+                    <ul class="navbar bg-body-tertiary" className="container">
 
-                <div>
-                    <ul class="navbar bg-body-tertiary">
-
-                        <li>
-                            <h2 class="navbar-brand">Pizza Di Sande</h2>
+                        <li className="lista">
+                            <a className="links" href="https://unsplash.com/es/fotos/gatito-blanco-y-gris-sobre-textil-blanco-so5nsYDOdxw"><h3>Cat Pizzas</h3></a>
                         </li>
 
-                        <li>
-                            <h3>Cat Pizzas</h3>
+                        <li className="lista">
+                            <a className="links" href="https://unsplash.com/es/fotos/gatito-atigrado-plateado-en-el-suelo-7AIDE8PrvA0"><h3>Cat Empanadas</h3></a>
                         </li>
 
-                        <li>
-                            <h3>Cat Empanadas</h3>
-                        </li>
-
-                        <li>
+                        <li className="lista">
                             <form class="d-flex" role="search">
                                 <input class="form-control me-2" type="Buscar" placeholder="Buscar" aria-label="Buscar" />
                                 <button class="btn btn-outline-success" type="submit">Buscar</button>
                             </form>
                         </li>
-
                     </ul>
                 </div>
             </nav>
